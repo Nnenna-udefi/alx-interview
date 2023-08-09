c08bc9c181dd043c63ecf128de1fcc137b875ed8
Nnenna udefi <miriamudefi@gmail.com>
2023-08-09 13:14:17 +0000

    mnimum operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Script involves finding the minimum number of operations required to
+    generate a specific number of 'H' characters in a text file using
+    the "Copy All" and "Paste" operations.
+"""
+
+
+def minOperations(n):
+    """
+        no_operations: number of operations needed to generate 'n' 'H'
+        characters
+        n: factors to determine number of operations
+    """
+    if n == 1:
+        return 0
+
+    no_operations = 0
+    divisor = 2
+
+    while n > 1:
+        if n % divisor == 0:
+            no_operations += divisor
+            n //= divisor
+        else:
+            divisor += 1
+    return no_operations
