ca81e5b698cb159002ad84a7fc1ccd7db070b520
Nnenna udefi <miriamudefi@gmail.com>
2023-09-08 20:16:57 +0000

    changed api link

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -7,7 +7,7 @@ const request = require('request');
 // as the first prositional argument
 const movieId = process.argv[2];
 
-const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
 
 request(url, (error, response, body) => {
   if (response.statusCode === 200 && !error) {
